7e2fc1131680af7602751e68b80e6038b6753f4a
boris <b.c.bergman@uu.nl>
2025-05-15 16:13:06 +0200

    qa

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ logger = logging.getLogger(__name__)
 UU_RESPIRATORY_UUID =  "d67b5503-a25c-4395-bd21-f92823d91442" 
 #UU_TRIGGER_UUID = "84b45e35-140a-4d32-92c6-386d8bff160d"
 BATTERY_LEVEL_UUID =    "00002A19-0000-1000-8000-00805f9b34fb"
-
-MAC_SAADC = "DA:88:4A:1C:07:70"
+SAMPLETIME = 16 #Freq: 16Mhz/8K OSR 128
+MAC_SAADC = "DA:8C:38:0C:36:29" #"DA:88:4A:1C:07:70"
 #MAC_TRIGGERBOX = "FB:41:2A:F4:5E:AA"
 
 class uutrack_reader:
@@ -34,11 +34,11 @@ class uutrack_reader:
         if self.counter == 0:
              self.starttime = time.time()                
         adc_values = struct.iter_unpack("<h", data)                
-        for adc_value in adc_values:            #12 bit values            
+        for adc_value in adc_values:            #14 bit values            
             #if(self.counter % 100 == 0):            
-            print(f'#: {self.counter:<8}time: {round((time.time() - self.starttime),3):<8} time_est: {self.counter*32:<8} value: {adc_value[0]:<8}')
+            print(f'#: {self.counter:<8} time: {self.counter*SAMPLETIME:<8} value: {adc_value[0]:<8}')
             self.counter+=1            
-            if(self.counter > 2500):
+            if(self.counter == 500):
                 print("stopping, sample ended")
                 self.semaphore.release()
                     
